6925cc564f31c40f9dd8ffa6bd65dd512d80fe74
Jeevan Joshi <117514522+JeevanJoshi4434@users.noreply.github.com>
2023-07-30 19:19:21 +0530

    V1.0.0

---
+++ README.md
@@ -0,0 +1,76 @@
+### otp-string-generator
+
+Description:
+
+The `otp-string-generator` module is a versatile utility for generating secure One-Time Passwords (OTPs) and random key strings in Node.js. It provides functions to create cryptographically secure random strings of specified lengths, with optional strict criteria and additional data. This module ensures high randomness and can be used for various use cases, such as generating authentication tokens, unique URLs, or any random data required in your Node.js applications.
+
+Code:
+
+```javascript
+const Generate = require('otp-string-generator');
+
+// Generate a 6-digit OTP
+const otp = Generate.getOTP(6);
+console.log(otp);
+// Output: "237834" or any other randomly generated 6-digit number string
+
+// Generate a random key string with parameters
+
+/**
+ * The function `getString` generates a random string of a specified length, with optional strict
+ * criteria and additional data.
+ * @param {number} Length - The length of the generated string. It specifies the number of characters the string should have.
+ * @param {boolean} Strict - Determines whether the generated string should follow strict rules or not. If `true`, the generated string will have a specific pattern. If `false`, the generated string will be random.
+ * @param {Object} more - An object that contains additional key-value pairs that you want to include in the generated string. These key-value pairs will be appended to the string in the format `key=value&`.
+ * @returns {string} A random string.
+ */
+const ExampleKey1 = Generate.getString(10, false, { _id: "10", email: "user@mail.com" });
+console.log(ExampleKey1);
+// Output: _id=10&email=user@mail.com&key=4cV2JM921B
+
+// Generate another random key string
+const ExampleKey2 = Generate.getString(10, false, { _id: "10", email: "user@mail.com" });
+console.log(ExampleKey2);
+// Output: _id=10&email=user@mail.com&key=L*Rq@EUoVn
+
+// Generate a URL key string with parameters
+
+/**
+ * The function `getUrlString` generates a URL string with specified length, strictness, and additional parameters.
+ * @param {number} Length - The length of the generated key string.
+ * @param {boolean} Strict - Determines whether the generated URL string should follow strict rules or not. If `Strict` is set to `true`, the generated URL string will include both lowercase and uppercase letters, as well as Special Characters `!@#$%^*`. If `Strict` is set to `false`, the generated URL string will be random.
+ * @param {Object} more - An object that contains additional key-value pairs to be included in the generated URL string. These key-value pairs will be appended to the URL as query parameters.
+ * @param {string} url - The base URL for the generated URL string.
+ * @param {boolean} https - A boolean value indicating whether the generated URL should use the "https" protocol or not.
+ * @returns {string} The generated URL string.
+ */
+const ExampleKey3 = Generate.getUrlString(10, false, { _id: "10", email: "user@mail.com" }, "www.example.com/verify?", false);
+console.log(ExampleKey3);
+// Output: http://www.example.com/verify??_id=10&email=user@mail.com&key=sLqTg@kGOS
+
+// Generate another URL key string with strictness and HTTPS
+
+const ExampleKey4 = Generate.getUrlString(10, true, { _id: "10", email: "user@mail.com" }, "www.example.com/verify?", true);
+console.log(ExampleKey4);
+// Output: https://www.example.com/verify??_id=10&email=user@mail.com&key=2#k!N%Q^y*
+```
+
+Other Points:
+
+1. The `otp-string-generator` module provides a convenient way to generate secure random strings, reducing the risk of predictable or weak tokens.
+
+2. The `getOTP` function is specifically designed to generate OTPs, which are commonly used for two-factor authentication and other security mechanisms.
+
+3. The `getString` function can be used to generate random strings with optional additional data. When `Strict` is set to `false`, the generated string will be random; when `true`, the string follows a specific pattern.
+
+4. The `getUrlString` function is tailored for generating URL keys, adding query parameters to the base URL. When `Strict` is set to `true`, the generated URL includes both lowercase and uppercase letters and special characters.
+
+5. For increased security, the module relies on the `crypto` module in Node.js to generate cryptographically secure random numbers.
+
+6. The generated strings can be used for a wide range of applications, such as token-based authentication, temporary URLs, and unique identifiers.
+
+Overall, the `otp-string-generator` module simplifies the process of generating secure random strings and OTPs in Node.js, making it an excellent addition to any application requiring random data.
+## Authors
+
+- [@Jeevan Joshi](https://www.github.com/JeevanJoshi4434)
+
---
+++ index.js
@@ -0,0 +1,180 @@
+/**
+ * The genOTP function generates a random string of numbers with a specified length.
+ * @param Length - The `Length` parameter is the desired length of the generated OTP (One-Time
+ * Password).
+ * @returns a randomly generated string of numbers with the specified length.
+ */
+function genOTP(Length){
+    if(typeof Length !== 'number' || Length <= 0){
+        throw new Error("TypeError: Length should be a 'Number' parameter and should be greater than '0'.");
+    }
+    let string = '';
+    for (let i = 0; i < Length; i++) {
+        string += getRandomNumber(0,9).toString();
+    }
+    return  string;
+}
+/**
+ * The function `genString` generates a random string of a specified length, with optional strict
+ * criteria and additional data.
+ * @param Length - The `Length` parameter determines the length of the generated string. It specifies
+ * the number of characters the string should have.
+ * @param Strict - The `Strict` parameter is a boolean value that determines whether the generated
+ * string should follow strict rules or not. If `Strict` is `true`, the generated string will have a
+ * specific pattern. If `Strict` is `false`, the generated string will be random.
+ * @param more - The `more` parameter is an object that contains additional key-value pairs that you
+ * want to include in the generated string. These key-value pairs will be appended to the string in the
+ * format `key=value&`.
+ * @returns a string.
+ */
+function genString(Length, Strict, more) {
+    if(typeof Strict !== "boolean"){
+        throw new Error("TypeError: Strict should be a 'Boolean' parameter.")
+    }
+    if(typeof more !== "object"){
+        throw new Error("TypeError: more should be an Object.");
+    }
+    if(typeof Length !== "number" || Length <= 0){
+        throw new Error("TypeError: Length should be a 'Number' parameter and should be greater than '0'.");
+    }
+    let string = '';
+    let Data = data(Strict);
+    let Args = [
+    ]
+    if (more.length !== 0) {
+        Args.push(more);
+        const objectWithId = Args[0]; // Access the first element of the array (index 0)
+        const fieldNames = Object.keys(objectWithId); // Get an array of property names
+
+        // Loop through the property names and access the corresponding values
+        for (const fieldName of fieldNames) {
+            const fieldValue = objectWithId[fieldName];
+            string += fieldName;
+            string += '=';
+            string += fieldValue;
+            string += '&';
+        }
+    }
+    if (!Strict) {
+        string += 'key=';
+        for (let i = 0; i < Length; i++) {
+            string += Data[getRandomNumber(0, Data.length - 1)];
+        }
+        return string;
+    } else {
+        string += 'key=';
+        for (let i = 0; i < Length/2; i++) {
+            string += Data[getRandomNumber(0, Data.length - 1)];
+            string += Data[getRandomNumber(62, Data.length -1)]
+        }
+        return string;
+    }
+}
+/**
+ * The function `genUrlString` generates a URL string with specified length, strictness, and additional
+ * parameters.
+ * @param Length - The length of the generated key string.
+ * @param Strict - The `Strict` parameter is a boolean value that determines whether the generated URL
+ * string should follow strict rules or not. If `Strict` is set to `true`, the generated URL string
+ * will include both lowercase and uppercase letters, as well as Special Character `!@#$%^*`. If `Strict` is set to
+ * `false`,
+ * @param more - The `more` parameter is an object that contains additional key-value pairs to be
+ * included in the generated URL string. These key-value pairs will be appended to the URL as query
+ * parameters.
+ * @param url - The `url` parameter is a string that represents the base URL for the generated URL
+ * string.
+ * @param https - A boolean value indicating whether the generated URL should use the "https" protocol
+ * or not.
+ * @returns The function `genUrlString` returns a URL string based on the provided parameters. The
+ * returned URL string will have the format `http://url?queryString` or `https://url?queryString`
+ * depending on the value of the `https` parameter.
+ */
+function genUrlString(Length, Strict, more,url,https) {
+    if(typeof Strict !== "boolean"){
+        throw new Error("TypeError: Strict should be a 'Boolean' parameter.")
+    }
+    if(typeof more !== "object"){
+        throw new Error("TypeError: more should be an Object.");
+    }
+    if(typeof Length !== "number" || Length <= 0){
+        throw new Error("TypeError: Length should be a 'Number' parameter and should be greater than '0'.");
+    }
+    if(typeof https !== 'boolean'){
+        throw new Error("TypeError: https should be a 'Boolean' parameter.");
+    }
+    if(typeof (url) !== 'string' || url.length === 0){
+        throw new Error(`TypeError: url must be a String and length should be greater than '0'.`)
+    }
+
+    let string = '';
+    let Data = data(Strict);
+    let Args = [
+    ]
+    if (more.length !== 0) {
+        Args.push(more);
+        const objectWithId = Args[0]; // Access the first element of the array (index 0)
+        const fieldNames = Object.keys(objectWithId); // Get an array of property names
+
+        // Loop through the property names and access the corresponding values
+        for (const fieldName of fieldNames) {
+            const fieldValue = objectWithId[fieldName];
+            string += fieldName;
+            string += '=';
+            string += fieldValue;
+            string += '&';
+        }
+    }
+    if (!Strict) {
+        string += 'key=';
+        for (let i = 0; i < Length; i++) {
+            string += Data[getRandomNumber(0, Data.length - 1)];
+        }
+        return (https ? 'https://' : 'http://') + url + '?' + string;
+    } else {
+        string += 'key=';
+        for (let i = 0; i < Length/2; i++) {
+            string += Data[getRandomNumber(0, Data.length - 1)];
+            string += Data[getRandomNumber(62, Data.length -1)]
+        }
+        return (https ? 'https://' : 'http://') + url + '?' + string;
+    }
+}
+
+/**
+ * The function returns an array containing lowercase letters, uppercase letters, numbers, and special
+ * characters.
+ * @param Strict - The "Strict" parameter is a boolean value that determines whether the generated
+ * character array should only contain lowercase letters, or if it should also include uppercase
+ * letters, numbers, and special characters. If "Strict" is set to true, the character array will only
+ * contain lowercase letters. If "Strict" is
+ * @returns The function `data` returns an array containing lowercase letters, uppercase letters,
+ * numbers, and special characters.
+ */
+const data = function (Strict) {
+    const characters = [...Array(26)].map((_, i) => String.fromCharCode(i + 65).toLowerCase());
+    const numbers = [...Array(10)].map((_, i) => i.toString());
+    const specialCharacters = '!@#$%^*';
+    const characterArray = characters.concat(characters.map(c => c.toUpperCase()), numbers, specialCharacters.split(''));
+    return characterArray;
+}
+/**
+ * The getRandomNumber function generates a random number within a specified range.
+ * @param from - The starting number of the range for generating a random number.
+ * @param to - The "to" parameter represents the upper limit of the range from which the random number
+ * will be generated.
+ * @returns The function getRandomNumber is returning a random number within the specified range (from
+ * and to).
+ */
+const getRandomNumber = function (from, to) { // for random number
+    if (from >= to) {
+        throw new Error('Invalid range: "from" must be less than "to"');
+    }
+    const randomNumber = Math.floor(Math.random() * (to - from + 1)) + from;
+    return randomNumber;
+}
+
+module.exports = {
+    getString:genString,
+    getUrlString:genUrlString,
+    getOTP:genOTP
+}
